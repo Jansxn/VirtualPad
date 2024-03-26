049a0d2a530442963eae17e902fe0f89ab28b0bb
Himanshu Banerji <himanshubanerji@gmail.com>
2024-03-26 18:15:36 +0530

    Server code changed to work with OSX along with requirement.txt

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ import math
 import socket
 
 
-wScr, hScr = 1536, 900 #Enter your screen resolution here. Get screen resolution from here - https://bestfirms.com/what-is-my-screen-resolution/
+wScr, hScr = (
+    1536,
+    900,
+)  # Enter your screen resolution here. Get screen resolution from here - https://bestfirms.com/what-is-my-screen-resolution/
 
-#HAND TRACKING MODULE ATTACHED
-class handDetector():
-    def __init__(self, mode=False, maxHands=1, complexity=1, detectionCon=0.7, trackCon=0.5):
+
+# HAND TRACKING MODULE ATTACHED
+class handDetector:
+    def __init__(
+        self, mode=False, maxHands=1, complexity=1, detectionCon=0.7, trackCon=0.5
+    ):
         self.mode = mode
         self.maxHands = maxHands
         self.complexity = complexity
@@ -17,7 +23,9 @@ class handDetector():
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.complexity, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(
+            self.mode, self.maxHands, self.complexity, self.detectionCon, self.trackCon
+        )
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
@@ -29,7 +37,9 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(
+                        img, handLms, self.mpHands.HAND_CONNECTIONS
+                    )
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
@@ -40,7 +50,7 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
-            # print(id, lm)
+                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 xList.append(cx)
@@ -55,24 +65,27 @@ class handDetector():
             bbox = xmin, ymin, xmax, ymax
 
             if draw:
-                cv2.rectangle(img, (xmin -20, ymin - 20), (xmax + 20, ymax + 20), (0, 255, 0), 2)
+                cv2.rectangle(
+                    img, (xmin - 20, ymin - 20), (xmax +
+                                                  20, ymax + 20), (0, 255, 0), 2
+                )
 
         return self.lmList, bbox
 
     def fingersUp(self):
         fingers = []
-    # Thumb
-        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] -1][1]:
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
-    # Fingers
+        # Fingers
         for id in range(1, 5):
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] -2][2]:
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
-    # totalFingers = fingers.count(1)
+        # totalFingers = fingers.count(1)
         return fingers
 
     def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
@@ -89,9 +102,10 @@ class handDetector():
 
         return length, img, [x1, y1, x2, y2, cx, cy]
 
+
 wCam, hCam = 640, 480
-frameR = 100     #Frame Reduction
-smoothening = 7  #random value
+frameR = 100  # Frame Reduction
+smoothening = 7  # random value
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -100,31 +114,32 @@ cap.set(4, hCam)
 detector = handDetector()
 option = -1
 finger_options = [
-    [0,1,0,0,0], #Clicking Mode
-    [0,0,0,0,1], #Right Click
-    [1,0,0,0,0], #Alt+tab
-    [0,1,1,0,0], #Moving Mode
-    [0,1,1,1,0], #Double Click Mode
-    [0,0,0,0,0], #Draggin Mode
+    [0, 1, 0, 0, 0],  # Clicking Mode
+    [0, 0, 0, 0, 1],  # Right Click
+    [1, 0, 0, 0, 0],  # Alt+tab
+    [0, 1, 1, 0, 0],  # Moving Mode
+    [0, 1, 1, 1, 0],  # Double Click Mode
+    [0, 0, 0, 0, 0],  # Draggin Mode
 ]
 
-HOST ='10.90.2.127'
-PORT=8485
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print('Socket created')
-s.bind((HOST,PORT))
-print('Socket bind complete')
+# HOST ='10.90.2.127'
+HOST = "192.168.205.120"
+PORT = 8485
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
+s.bind((HOST, PORT))
+print("Socket bind complete")
 s.listen(10)
-print('Socket now listening')
+print("Socket now listening")
 
-conn,addr=s.accept()
+conn, addr = s.accept()
 
 while True:
     # Find the landmarks
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
-    
+
     # Get the tip of fingers
     if len(lmList) != 0:
         x0, y0 = lmList[4][1:]
@@ -132,25 +147,28 @@ while True:
         x2, y2 = lmList[12][1:]
         x3, y3 = lmList[16][1:]
         x4, y4 = lmList[20][1:]
-        
+
         # Check which fingers are up
         fingers = detector.fingersUp()
-        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-                      (255, 0, 255), 2)
-        
+        cv2.rectangle(
+            img, (frameR, frameR), (wCam - frameR,
+                                    hCam - frameR), (255, 0, 255), 2
+        )
+
         try:
             option = finger_options.index(fingers)
         except ValueError:
             option = -1
         # Convert option to bytes and send it to the client
-        data = f"{option+1},{x1},{y1}"
+        data = f"{option+1},{x1},{y1},"
+        print(data)
         conn.sendall(data.encode())
 
     # Display
     img1 = cv2.flip(img, 1)
     cv2.imshow("Cam", img1)
-    if (cv2.waitKey(1) & 0xFF == ord('d')):
+    if cv2.waitKey(1) & 0xFF == ord("d"):
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
